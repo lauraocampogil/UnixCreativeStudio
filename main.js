f61d3286c9c077cca3bb5c058746e361e0b085e5
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-01-23 18:06:24 +0100

    animation-for-contact-us-and-for-index

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ faqs.forEach((faq) => {
 		faq.classList.toggle("active");
 	});
 });
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+		} else {
+			entry.target.classList.remove("show");
+		}
+	});
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
