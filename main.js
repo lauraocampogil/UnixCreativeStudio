94f5fb4e6daeec70fdb18d3167c2c2f66bc8e2dc
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-02-28 14:21:06 +0100

    hamburgermenu and faq javascript in html for better speed

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// faq
-const faqs = document.querySelectorAll(".faq");
-faqs.forEach((faq) => {
-	faq.addEventListener("click", () => {
-		faq.classList.toggle("active");
-	});
-});
-
 // animation scroll
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
@@ -19,32 +11,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
-
-// hamburgermenu
-const hamburger = document.querySelector(".hamburger-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const closeMenu = document.querySelector(".close-menu");
-const mobileLinks = document.querySelectorAll(".mobile-menu-links a");
-
-hamburger.addEventListener("click", () => {
-	mobileMenu.classList.add("active");
-	document.body.style.overflow = "hidden";
-	document.querySelector(".nav-container").style.display = "none";
-});
-
-closeMenu.addEventListener("click", () => {
-	mobileMenu.classList.remove("active");
-	document.body.style.overflow = "auto";
-	document.querySelector(".nav-container").style.display = "flex";
-});
-
-mobileLinks.forEach((link) => {
-	link.addEventListener("click", () => {
-		mobileMenu.classList.remove("active");
-		document.body.style.overflow = "auto";
-		document.querySelector(".nav-container").style.display = "flex";
-	});
-});
 
 // Translation functionality
 let translations = {};
